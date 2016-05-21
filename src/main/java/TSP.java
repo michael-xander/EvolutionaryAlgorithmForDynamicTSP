ec51f06909eaae98fd358b851074d194be724712
Michael <michaelkyeyune01@gmail.com>
2016-05-21 15:23:08 +0200

    Adjusted evolve to utilise the original cities for mutation

--- src/main/java/TSP.java
+++ src/main/java/TSP.java
@@ -110,7 +110,7 @@ public class TSP {
 
         for(int i = 0; i < numberOfMutations; i++)
         {
-            Chromosome mutant = parent.mutateWithInversion(cities);
+            Chromosome mutant = parent.mutateWithInversion(originalCities);
 
             for(int j = 0; j < chromosomes.length; j++)
             {
